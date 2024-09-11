7afe2732bcc8eabf04df0da820b127365136cfec
Gustavo Vicente Ozorio <gustavoozorio1227@hotmail.com>
2024-09-11 13:40:01 -0300

    Data Frame

---
+++ DataFrame.py
@@ -0,0 +1,37 @@
+import pandas as pd 
+
+
+data = pd.read_csv('./dataset/2004-2021.tsv', sep=';')
+
+type(data)
+data.shape
+
+#print(f'o data fra possui {data.shape[0]} linha/observação/registros e {data.shape[1]} colunas/ atributos/variaveiss.')
+
+#criando dataFrame
+
+personagfens_df = pd.DataFrame({
+    'nome': ['Luke','yoda','robo'],
+    'idade':[10,20,30],
+    'peso':[70.5,60.5,100.02],
+    'ehjedi': [True, True, False]
+})
+#personagfens_df.info()
+#personagfens_df.columns
+#print(personagfens_df)
+lista = list(personagfens_df.columns)
+print(lista) 
+
+# perosnagens_df_renomeado = personagfens_df.rename(columns={
+#         'nome': 'Nome Completo',  # renomeia a coluna de nome 'nome' para 'Nome completo
+#         'idade': 'Idade'
+
+# })
+
+# print(perosnagens_df_renomeado)
+
+# personagfens_df.rename(columns={'nome': 'Nome Completo','idade': 'Idade'}, inplace=True)
+# print(personagfens_df)
+
+personagfens_df.columns = ['NOME','IDADE','PESO', 'EH JEDI']
+print(personagfens_df)
--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import pandas as pd
 # Importando um dataset
 
 data = pd.read_csv('./dataset/2004-2021.tsv', sep=';')
-data.head()
+data.info()
