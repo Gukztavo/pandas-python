dc8b8fd7f6cae3913783bf98556c43ca71921c61
Gustavo Vicente Ozorio <gustavoozorio1227@hotmail.com>
2024-09-16 10:32:02 -0300

    usando iloc/loc

--- indices.py
+++ indices.py
@@ -12,5 +12,19 @@ pesquisa_de_satisfacao = pd.DataFrame({
     'pessimo':[30,20,1]
 }, index=['XboxOne', 'Playstation4','Switch'])
 
-print(pesquisa_de_satisfacao.index)
-pesquisa_de_satisfacao.index
+# print(pesquisa_de_satisfacao.index)
+# pesquisa_de_satisfacao.index
+
+data.iloc[[1,5,10,15]]  #slice
+
+#selecionaar linhas/obs de indices 5,1,15,10  
+
+data.iloc[[5,1,15,10]]
+
+#retonar valor indice 1 e coluna 4
+data.iloc[1,4]
+
+
+print(pesquisa_de_satisfacao.iloc[0,1])
+
+print(pesquisa_de_satisfacao.loc['XboxOne'])
